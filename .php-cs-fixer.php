629d90c99116592eee6ae8280d041e4e94e533a5
Georgi Kurian <georgi@worksitesafety.ca>
2022-11-07 20:20:43 -0500

    Lintier config updated

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,12 +1,20 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->exclude('somedir')
     ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
-    ->in(__DIR__)
-;
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/domain',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php');
 
 $config = new PhpCsFixer\Config();
+
 return $config->setRules([
         '@PSR12' => true,
         'strict_param' => true,
