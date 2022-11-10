43f9b6b81dfa109b1e6b96958582ae0075f048f4
Georgi Kurian <georgi@worksitesafety.ca>
2022-11-10 09:47:54 -0500

    Fixing export

--- domain/Strategy1Analysics/Exports/SimulationExport.php
+++ domain/Strategy1Analysics/Exports/SimulationExport.php
@@ -7,14 +7,17 @@ use App\Models\Day;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class SimulationExport implements FromQuery, WithHeadings
+class SimulationExport implements FromQuery, WithHeadings, WithMapping
 {
     use Exportable;
 
     public function query()
     {
-        return Day::query()->orderBy('day_index')->orderBy('time');
+        return Day::query()
+            ->with(['candleSticks'])
+            ->orderBy('day_index');
     }
 
     public function headings(): array
@@ -31,21 +34,26 @@ class SimulationExport implements FromQuery, WithHeadings
         ];
     }
 
-    public function map(Day $day): array
+    /**
+     * 
+     * @param Day $day 
+     */
+    public function map($day): array
     {
         return $day
-            ->candleSticks()
+            ->candleSticks
             ->map(function (CandleStick $candleStick) use ($day) {
                 return [
                     $candleStick->day_index,
                     $candleStick->time,
                     $candleStick->high,
                     $candleStick->low,
-                    $candleStick->id === $day->long_end_at_candle_stick_id ? $day->long_enter_at_price : null,
-                    $candleStick->id === $day->long_exit_at_candle_stick_id ? $day->long_exit_at_price : null,
-                    $candleStick->id === $day->short_end_at_candle_stick_id ? $day->short_enter_at_price : null,
-                    $candleStick->id === $day->short_exit_at_candle_stick_id ? $day->short_exit_at_price : null,
+                    ($candleStick->id === $day->long_end_at_candle_stick_id) ? $day->long_enter_at_price : null,
+                    ($candleStick->id === $day->long_exit_at_candle_stick_id) ? $day->long_exit_at_price : null,
+                    ($candleStick->id === $day->short_start_at_candle_stick_id) ? $day->short_enter_at_price : null,
+                    ($candleStick->id === $day->short_end_at_candle_stick_id) ? $day->short_exit_at_price : null,
                 ];
-            });
+            })
+            ->toArray();
     }
 }
