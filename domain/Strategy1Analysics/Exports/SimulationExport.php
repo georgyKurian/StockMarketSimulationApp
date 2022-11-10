5fc9e480d0ebf628a38fd94148cadff150303183
Georgi Kurian <georgi@worksitesafety.ca>
2022-11-09 20:39:39 -0500

    Added the export code

--- domain/Strategy1Analysics/Exports/SimulationExport.php
+++ domain/Strategy1Analysics/Exports/SimulationExport.php
@@ -22,9 +22,34 @@ class SimulationExport implements FromQuery, WithHeadings
     public function headings(): array
     {
         return [
-            '#',
-            'User',
-            'Date',
+            'Day',
+            'Time',
+            'High',
+            'Low',
+            'Long Enter',
+            'Long Exit',
+            'Short Enter',
+            'Short Exit',
         ];
     }
+
+    
+    public function map(Day $day): array
+    {
+        return $day
+            ->candleSticks()
+            ->map(function(CandleStick $candleStick) use($day) {
+                return [
+                    $candleStick->day_index,
+                    $candleStick->time,
+                    $candleStick->high,
+                    $candleStick->low,
+                    $candleStick->id === $day->long_end_at_candle_stick_id ? $day->long_enter_at_price : null,
+                    $candleStick->id === $day->long_exit_at_candle_stick_id ? $day->long_exit_at_price : null,
+                    $candleStick->id === $day->short_end_at_candle_stick_id ? $day->short_enter_at_price : null,
+                    $candleStick->id === $day->short_exit_at_candle_stick_id ? $day->short_exit_at_price : null,
+                ];
+            });
+    }
+
 }
